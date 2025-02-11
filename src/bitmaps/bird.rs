a484a7619ac0b32f35e072809afc273afd97660a
Felix Wolfsteller <felix.wolfsteller@mailbox.org>
2025-02-11 21:43:21 +0100

    add omitted bird bitmap

---
+++ src/bitmaps/bird.rs
@@ -0,0 +1,21 @@
+const ˑ: bool = true;
+const Ø: bool = false;
+
+#[rustfmt::skip]
+pub const BIRD_BITMAP: [[bool; 3]; 3] = [
+    [ˑ, Ø, ˑ],
+    [Ø, ˑ, Ø],
+    [Ø, ˑ, Ø],
+];
+#[rustfmt::skip]
+pub const BIRD_BITMAP2: [[bool; 3]; 3] = [
+    [Ø, Ø, Ø],
+    [ˑ, ˑ, ˑ],
+    [Ø, ˑ, Ø],
+];
+#[rustfmt::skip]
+pub const BIRD_BITMAP3: [[bool; 3]; 3] = [
+    [Ø, Ø, Ø],
+    [Ø, ˑ, Ø],
+    [ˑ, Ø, ˑ],
+];
